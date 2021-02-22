b42f50fb2719ab8edba031062fcbb30fb86b8865
Timothy Sanderson <t.s.sanderson@hotmail.co.uk>
2021-02-22 17:12:43 +0000

    Can make VisualLore interactables
    Visual lore allow you to associate an image and a picture together which then displays itself using the VisualLoreCanvas prefab when interacted with.

--- ToxicHospitalUnity/Assets/Scripts/PlayerInteractor.cs
+++ ToxicHospitalUnity/Assets/Scripts/PlayerInteractor.cs
@@ -18,6 +18,10 @@ public class PlayerInteractor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (VisualLoreCanvasManager.Instance.isActiveAndEnabled)
+        {
+            return;
+        }
         if (Input.GetButtonDown("Interact"))
         {
             if(interactables.Count == 0)
