935f77e425ce42c890e926d93d1624d80819afa9
Daniel Korsah <danielkorsah97@gmail.com>
2021-02-27 17:05:14 +0000

    Remove debug logs

--- ToxicHospitalUnity/Assets/Scripts/Interactable/CheckpointInteractable.cs
+++ ToxicHospitalUnity/Assets/Scripts/Interactable/CheckpointInteractable.cs
@@ -36,15 +36,14 @@ public class CheckpointInteractable : BaseInteractable
         SaveInteractableData();
         SaveEnemyData();
 
-        Debug.Log("Checkpoint activated");
-        Debug.Log(string.Format("Player Infection: {0}", CheckpointStatus.PlayerInfectionLevel));
-        Debug.Log(string.Format("Player Health(%): {0}", CheckpointStatus.PlayerHealthPercentage));
-        Debug.Log(string.Format("Player transform.position: {0}", CheckpointStatus.PlayerTransform.position));
-        Debug.Log(string.Format("Inventory Audio Logs: {0}", CheckpointStatus.Recordings));
-        Debug.Log(string.Format("Player Infection: {0}", CheckpointStatus.PlayerInfectionLevel));
-        Debug.Log(string.Format("Interactable list, element 0, name: {0}", CheckpointStatus.AllInteractables[0].name));
-        Debug.Log(string.Format("Interactable transfroms list, element 0, position", CheckpointStatus.AllInteractablesTransforms));
-        Debug.Log(string.Format("Interactable activity statuses list: {0}", CheckpointStatus.ActivyStatuses));
+        // Debug.Log("Checkpoint activated");
+        // Debug.Log(string.Format("Player Infection: {0}", CheckpointStatus.PlayerInfectionLevel));
+        // Debug.Log(string.Format("Player Health(%): {0}", CheckpointStatus.PlayerHealthPercentage));
+        // Debug.Log(string.Format("Player transform.position: {0}", CheckpointStatus.PlayerTransform.position));
+        // Debug.Log(string.Format("Inventory Audio Logs: {0}", CheckpointStatus.Recordings));
+        // Debug.Log(string.Format("Interactable list, element 0, name: {0}", CheckpointStatus.AllInteractables[0].name));
+        // Debug.Log(string.Format("Interactable transfroms list, element 0, position: {0}", CheckpointStatus.AllInteractablesTransforms[0].position));
+        // Debug.Log(string.Format("Interactable activity statuses list, element 0: {0}", CheckpointStatus.ActivyStatuses[0]));
     }
 
     private void SavePayerData()
