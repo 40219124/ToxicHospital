822746e71e6f5cd7399b47c24fb4f3bb3f183d89
Daniel Korsah <danielkorsah97@gmail.com>
2021-02-27 14:19:52 +0000

    Set up CheckpointInteractable inherited class.

---
+++ ToxicHospitalUnity/Assets/Scripts/Interactable/CheckpointInteractable.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointInteractable : BaseInteractable
+{
+    protected override void DoInteractionAction()
+    {
+
+    private void CheckpointPayerData()
+    {
+
+    private void CheckpointInventoryData()
+    {
+
+    }
+
+    private void CheckpointInteractableData()
+    {
+
+    }
+
+    private void CheckpointEnemyData()
+    {
+        //TODO enemies don't exist yet
+    }
+}
+
