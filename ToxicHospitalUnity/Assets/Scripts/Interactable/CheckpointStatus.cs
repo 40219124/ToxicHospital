6969e55383dc597d5678863fcc7661898249e18e
Daniel Korsah <danielkorsah97@gmail.com>
2021-02-27 16:44:36 +0000

    Reorganise a little bit

--- ToxicHospitalUnity/Assets/Scripts/Interactable/CheckpointStatus.cs
+++ ToxicHospitalUnity/Assets/Scripts/Interactable/CheckpointStatus.cs
@@ -11,15 +11,14 @@ public static class CheckpointStatus
     public static float PlayerInfectionLevel;
     public static float PlayerHealthPercentage;
 
+    //store the loaction, scale, and rotation at time of checkpoint
+    public static Transform PlayerTransform;
+
 
     //store inventory contents
     public static List<AudioLore> Recordings = new List<AudioLore>();
     public static List<VisualLore> Letters = new List<VisualLore>();
     public static List<VisualLore> Reports = new List<VisualLore>();
-
-
-    //store the loaction, scale, and rotation at time of checkpoint
-    public static Transform PlayerTransform;
 
 
     // get a reference to all interactables and their transforms(for the resetting movable ones) and their active/inactive bool.
