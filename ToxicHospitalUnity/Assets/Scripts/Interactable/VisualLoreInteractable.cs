0964486f7a958741b20f0d54ed75e021586327d7
Timothy Sanderson <t.s.sanderson@hotmail.co.uk>
2021-02-28 02:06:48 +0000

    Particles for lore interactables

--- ToxicHospitalUnity/Assets/Scripts/Interactable/VisualLoreInteractable.cs
+++ ToxicHospitalUnity/Assets/Scripts/Interactable/VisualLoreInteractable.cs
@@ -6,8 +6,22 @@ public class VisualLoreInteractable : BaseInteractable
 {
     [SerializeField]
     protected VisualLore visualLore;
+
+    protected bool interactedWith = false;
+    protected ParticleSystem particles;
+
+    protected override void Start()
+    {
+        base.Start();
+        particles = GetComponentInChildren<ParticleSystem>();
+    }
     protected override void DoInteractionAction()
     {
         VisualLoreCanvasManager.Instance.UpdateAndOpen(visualLore);
+        if (!interactedWith)
+        {
+            interactedWith = true;
+            particles.Stop();
+        }
     }
 }
