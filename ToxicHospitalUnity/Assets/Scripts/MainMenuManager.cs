5d049fc07a10479af4ec4689537bd1940a2e7acd
Timothy Sanderson <t.s.sanderson@hotmail.co.uk>
2021-02-28 17:29:06 +0000

    Select highlighted menu options. Title screen progress.

--- ToxicHospitalUnity/Assets/Scripts/MainMenuManager.cs
+++ ToxicHospitalUnity/Assets/Scripts/MainMenuManager.cs
@@ -1,11 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class MainMenuManager : MonoBehaviour
 {
+    [SerializeField]
+    private Selectable startOption;
+
+    private void Start()
+    {
+        startOption.Select();
+    }
+
     public void UIOpenGame()
     {
         BootManager.Instance.GoToGameScene();
+    }
+
+    public void UIOpenSettings()
+    {
+        // ~~~ open settings screen
+    }
+
+    public void UIQuitGame()
+    {
+        Application.Quit();
     }
 }
--- ToxicHospitalUnity/Assets/Scripts/SelectedMenuFeedback.cs
+++ ToxicHospitalUnity/Assets/Scripts/SelectedMenuFeedback.cs
@@ -1,10 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class SelectedMenuFeedback : MonoBehaviour, ISelectHandler
+[RequireComponent(typeof(Selectable))]
+public class SelectedMenuFeedback : MonoBehaviour, ISelectHandler, IPointerEnterHandler
 {
+    private Selectable selectable;
+
+    private void Start()
+    {
+        selectable = GetComponent<Selectable>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        selectable.Select();
+    }
+
     public void OnSelect(BaseEventData eventData)
     {
         MenuSFXController.Instance.OnItemSelected();
