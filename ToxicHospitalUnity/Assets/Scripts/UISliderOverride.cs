8bbe0198d119d75f43b873dc9b682bc69cc93b76
Timothy Sanderson <t.s.sanderson@hotmail.co.uk>
2021-02-28 16:05:09 +0000

    Fix ui selected status

--- ToxicHospitalUnity/Assets/Scripts/UISliderOverride.cs
+++ ToxicHospitalUnity/Assets/Scripts/UISliderOverride.cs
@@ -11,7 +11,7 @@ public class UISliderOverride : UISlider
     //Only care about if it's already showing, not if any UISliders are showing. Ignore UILock
     public override void Show()
     {
-
+        SelectSelectable();
         //show this individual
         LeanTween.cancel(gameObject);
         LeanTween.move(rect, showingPosition, easeTime).setEase(LeanTweenType.easeOutQuint).setOnComplete(() => { rect.localPosition = showingPosition; });
@@ -22,6 +22,7 @@ public class UISliderOverride : UISlider
 
     public override void Hide()
     {
+        DeselectSelectable();
         //hide this individual
         LeanTween.cancel(gameObject);
         LeanTween.move(rect, hiddenPosition, easeTime).setEase(LeanTweenType.easeOutQuint).setOnComplete(() => { rect.localPosition = hiddenPosition; });
